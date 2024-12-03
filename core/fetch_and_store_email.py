6bd9242f39af8da1ccb096a106d8e6eeb3bc6e25
GuruVi <guru.sivasubramanian@selfscore.com>
2024-12-03 21:12:41 +0530

    Adds fetch and store process

---
+++ core/fetch_and_store_email.py
@@ -0,0 +1,46 @@
+""" This module fetches the email from Gmail API and stores it in the database. """
+
+
+import datetime
+import logging
+from typing import Any
+
+from core.pydantic_models import EmailData
+from core.services.db.email_service import create_email
+from core.services.gmail.client import get_email_message, list_email
+
+LOGGER: logging.Logger = logging.getLogger(name=__name__)
+
+# Mapping header names to actions
+HEADER_HANDLERS: dict[str, Any] = {
+    "From": lambda fields, value: fields.update({"from_address": value}),
+    "To": lambda fields, value: fields.update({"to_address": value}),
+    "Subject": lambda fields, value: fields.update({"subject": value}),
+    "Received": lambda fields, value: fields.update({
+        "date_received": (datetime.datetime.strptime(((value.split(';')[1]).strip()).split(" (")[0], "%a, %d %b %Y %H:%M:%S %z")).astimezone(datetime.timezone.utc)
+    }),
+}
+
+def process(*, user_id: str):
+    """     
+    Fetch and store email from Gmail API.
+
+    :param user_id: User's email id
+    :type user_id: str
+    """
+    LOGGER.info("Fetching email from Gmail API")
+    email_list = list_email(user_id=user_id)
+    for email in email_list["messages"]:
+        email_message = get_email_message(user_id=user_id, message_id=email["id"])
+        # Process email headers
+        email_data_fields: dict[str, Any] = {}
+        print(email_message["id"])
+        for header in email_message["payload"]["headers"]:
+            name: str = header.get("name")
+            value: Any = header.get("value")
+            if name in HEADER_HANDLERS:
+                HEADER_HANDLERS[name](email_data_fields, value)
+        email_data_fields.update({'gmail_message_id': email_message["id"]})
+        email_data = EmailData(**email_data_fields)
+        create_email(email=email_data)
+    LOGGER.info("Email has been successfully fetched and stored in the database.")
---
+++ tests/test_fetch_and_store_email.py
@@ -0,0 +1,61 @@
+import datetime
+from unittest import mock
+import pytest
+
+from core.fetch_and_store_email import process
+from gmail_rules_engine.tables import Email
+
+pytestmark = pytest.mark.fetch_and_store_email
+
+
+@mock.patch("core.fetch_and_store_email.list_email")
+@mock.patch("core.fetch_and_store_email.get_email_message")
+def test_should_fetch_and_store_email(get_email_message_mock, list_email_mock):
+    list_email_mock.return_value = {
+        "messages": [
+            {
+                "id": "19380f65a0cd3791",
+                "threadId": "19380664d0cd9a9b"
+            }
+        ]
+    }
+    get_email_message_mock.return_value = {
+        "id": "19380f65a0cd3791",
+        "threadId": "19380664d0cd9a9b",
+        "labelIds": ["CATEGORY_PERSONAL", "INBOX"],
+        "snippet": "Dear Sir/Madam,",
+        "payload": {
+            "partId": "",
+            "mimeType": "multipart/mixed",
+            "filename": "",
+            "headers": [
+                {"name": "Delivered-To", "value": "abcde@gmail.com"},
+                {
+                    "name": "Received",
+                    "value": "by 2002:a05:640c:1501:b0:219:8a79:a0ea with SMTP id n1csp2837164eir;        Tue, 3 Dec 2024 01:47:41 -0800 (PST)",
+                },
+                {"name": "Date", "value": "Sun, 1 Dec 2024 11:36:27 +0530 (IST)"},
+                {"name": "From", "value": "kblalerts@ktkbank.in"},
+                {"name": "To", "value": "abcde@gmail.com"},
+                {
+                    "name": "Subject",
+                    "value": "****** BANK- Transaction EMAIL Alert ******",
+                },
+                {"name": "Mime-Version", "value": "1.0"},
+            ],
+            "body": {"size": 0},
+            "parts": [],
+        },
+        "sizeEstimate": 6144,
+        "historyId": "9474857",
+        "internalDate": "1733033187000",
+    }
+    
+    process(user_id='abcd@gmail.com')
+
+    email: Email = Email.objects().get(Email.gmail_message_id == "19380f65a0cd3791").run_sync()
+    assert email.gmail_message_id == "19380f65a0cd3791"
+    assert email.from_address == "kblalerts@ktkbank.in"
+    assert email.to_address == "abcde@gmail.com"
+    assert email.date_received == datetime.datetime(year=2024, month=12, day=1, hour=6, minute=6, second=27, tzinfo=datetime.timezone.utc)
+    assert email.subject == "****** BANK- Transaction EMAIL Alert ******"
