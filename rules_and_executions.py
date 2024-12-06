3b7e0e174baa14c0b97846c7d4603b44e79c1886
guruvi <guruvigneshwaran@gmail.com>
2024-12-06 22:18:21 +0530

    REmoves incorrect params

--- rules_and_executions.py
+++ rules_and_executions.py
@@ -5,13 +5,11 @@ from core.rules_executions import execute
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: pdm run apply-rules <user_id> <page_size> <number_of_pages>")
+    if len(sys.argv) < 2:
+        print("Usage: pdm run apply-rules <user_id>")
         sys.exit(1)
     user_id = sys.argv[1]
     execute(
         user_id=user_id,
-        pagee_size=sys.argv[2],
-        number_of_pages=sys.argv[3],
         config_dict=rules_config.sample_rule,
     )
