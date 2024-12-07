cd768d2cd50e407fec06d0ab437f3a073ac817bc
guruvi <38998877+guruvi@users.noreply.github.com>
2024-12-07 17:25:55 +0530

    Update rules_and_executions.py

--- rules_and_executions.py
+++ rules_and_executions.py
@@ -4,12 +4,12 @@ import rules_config
 from core.rules_executions import execute
 
 
-# if __name__ == "__main__":
-#     if len(sys.argv) < 2:
-#         print("Usage: pdm run apply-rules <user_id>")
-#         sys.exit(1)
-#     user_id = sys.argv[1]
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: pdm run apply-rules <user_id>")
+        sys.exit(1)
+    user_id = sys.argv[1]
 execute(
-    user_id='guruvigneshwaran@gmail.com',
+    user_id=user_id,
     config_dict=rules_config.sample_rule,
 )
